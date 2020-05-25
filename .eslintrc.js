2802f0d070a7f04f767e2338feda75b4b7c5f8a3
Tran Minh Quang <quangtm210395@gmail.com>
2020-05-25 20:05:50 +0700

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: 'airbnb-base',
+  parserOptions: {
+    ecmaVersion: 8,
+    sourceType: 'module',
+  },
+  rules: {
+    semi: 'error',
+  },
+};
