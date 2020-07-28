f5514f1e4aee65233359739960c1d0338b528d30
Tran Minh Quang <quangtm210395@gmail.com>
2020-07-28 22:26:56 +0700

    update

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+  const { headers } = req;
+  if (!headers['user-agent']) {
+    return res.json({
+      message: 'user-agent header is require',
+    });
+  }
+  return next();
+});
+
 app.get('/', (req, res) => {
   console.log(req.params);
   res.send('hello');
